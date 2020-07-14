2b3e49062dbc144335de3eeb841362d7e428c9cc
Otávio Pace <otaviopp8@gmail.com>
2020-07-14 02:28:37 -0300

    components: move servlet to its own namespace

--- src/dodecagon/components.clj
+++ src/dodecagon/components.clj
@@ -1,33 +1,16 @@
 (ns dodecagon.components
   (:require [com.stuartsierra.component :as component]
-            [io.pedestal.http :as pedestal]
             [dodecagon.components.config :refer [new-config]]
             [dodecagon.components.routes :refer [new-routes]]
             [dodecagon.components.service :refer [new-service]]
-            [dodecagon.service :as service]))
-
-
-(defrecord Servlet [service]
-  component/Lifecycle
-  (start [this]
-    (assoc
-      this
-      :instance
-      (-> service
-          :runnable-service
-          (assoc ::pedestal/join? false)
-          pedestal/create-server
-          pedestal/start)))
-  (stop [this]
-    (pedestal/stop (:instance this))
-    (dissoc this :instance)))
+            [dodecagon.components.servlet :refer [new-servlet]]))
 
 (def base-system
   (component/system-map
     :config (new-config)
     :routes (new-routes)
     :service (component/using (new-service) [:config :routes])
-    :servlet (component/using (->Servlet {}) [:service])))
+    :servlet (component/using (new-servlet) [:service])))
 
 (def systems-map
   {:base base-system})
---
+++ src/dodecagon/components/servlet.clj
@@ -0,0 +1,21 @@
+(ns dodecagon.components.servlet
+  (:require [com.stuartsierra.component :as component]
+            [io.pedestal.http :as pedestal]))
+
+(defrecord Servlet [service]
+  component/Lifecycle
+  (start [this]
+    (assoc
+      this
+      :instance
+      (-> service
+          :runnable-service
+          (assoc ::pedestal/join? false)
+          pedestal/create-server
+          pedestal/start)))
+  (stop [this]
+    (pedestal/stop (:instance this))
+    (dissoc this :instance)))
+
+(defn new-servlet []
+  (->Servlet {}))
