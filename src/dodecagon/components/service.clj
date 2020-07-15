4a369a995fe3e4594474614046a5e4afffb7ddf2
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 14:24:38 -0300

    interceptors: add components map as property on request

--- src/dodecagon/components/service.clj
+++ src/dodecagon/components/service.clj
@@ -1,5 +1,6 @@
 (ns dodecagon.components.service
   (:require [com.stuartsierra.component :as component]
+            [io.pedestal.interceptor.helpers :refer [before]]
             [io.pedestal.http :as pedestal]
             [io.pedestal.http.route :as route]))
 
@@ -13,6 +14,13 @@
      ::pedestal/secure-headers  {:content-security-policy-settings {:object-src "none"}}
      ::pedestal/allowed-origins {:creds true :allowed-origins (constantly true)}}))
 
+(defn add-components-interceptor [service]
+  (before (fn [context] (assoc-in context [:request :components] service))))
+
+(defn add-custom-interceptors [service-map service]
+  (let [custom-interceptors [(add-components-interceptor service)]]
+    (update service-map ::pedestal/interceptors concat custom-interceptors)))
+
 (defn runnable-service [service-map config routes]
   (let [base-service (if (= (:environment config) :prod)
                        prod-service
@@ -22,11 +30,9 @@
                 ::pedestal/routes          #(route/expand-routes (deref routes))
                 ::pedestal/resource-path   "/public"
                 ::pedestal/type            :jetty
-                ::pedestal/port            (:dev-port config)
-                ;; TODO: add system-interceptor
-                ;; ::pedestal/interceptors    (update )
-                })
-        pedestal/default-interceptors)))
+                ::pedestal/port            (:dev-port config)})
+        pedestal/default-interceptors
+        (add-custom-interceptors service-map))))
 
 (defrecord Service [config routes]
   component/Lifecycle
